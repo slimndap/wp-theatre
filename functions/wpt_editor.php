ef5c9a6329dc20027ac46706db6fa12533542f0c
Jeroen Schmit <jeroen@slimndap.com>
2014-06-17 10:17:50 +0200

    Juggling filters and sorting navigation around admin page.

--- functions/wpt_editor.php
+++ functions/wpt_editor.php
@@ -5,23 +5,27 @@
 			add_action( 'admin_enqueue_scripts', array($this,'admin_enqueue_scripts'), 20);
 
 			add_action( 'wp_ajax_productions',array($this,'ajax_productions'));
-
+			add_filter( 'admin_footer_text',array($this,'admin_footer_text'));
+			
+			$this->page = false;
 		}
 		
 		function admin_enqueue_scripts() {
 			global $wp_theatre;
-			wp_localize_script(
-				'wpt_admin',
-				'wpt_editor_ajax',
-				array( 
-					'url' => admin_url( 'admin-ajax.php' ),
-					'order_key' => $wp_theatre->order->meta_key
-				) 
-			);
+			if ($this->is_theater_admin()) {
+				wp_localize_script(
+					'wpt_admin',
+					'wpt_editor_ajax',
+					array( 
+						'url' => admin_url( 'admin-ajax.php' ),
+						'order_key' => $wp_theatre->order->meta_key
+					) 
+				);
+			}
 		}
 		
 		function admin_menu() {
-			add_submenu_page(
+			$this->page = add_submenu_page(
 				'theatre',
 				__('Theater for WordPress','wp_theatre'),
 				'WPT Editor',
@@ -40,10 +44,16 @@
     		// create event form
 			echo $this->production_form();
 			
+			// sort
+			echo '<div class="wpt_editor_sort"> Sort by:';
+			echo '<span class="sort" href="#" data-sort="title">name</span>';
+			echo '<span class="sort" href="#" data-sort="'.$wp_theatre->order->meta_key.'">date</span>';
+			echo '</div>';
+
 			// filters
+			echo '<div class="wpt_editor_filters">';
 			echo '<input class="search" />';
-			echo '<span class="sort" data-sort="title">Sort by name</span>';
-			echo '<span class="sort" data-sort="'.$wp_theatre->order->meta_key.'">Sort by city</span>';
+			echo '</div>';
 			
 			// settings
 			
@@ -87,12 +97,27 @@
 			wp_send_json($wp_theatre->productions->to_array($args));
 		}
 		
+		function is_theater_admin() {
+			$screen = get_current_screen();
+			return $this->page && !empty($screen) && ($screen->id == $this->page);
+		}
+		
 		function production_form() {
 			echo '<form id="wpt_editor_create_production_form">';
 			
 			echo '<input type="text" placeholder="'.__('Start typing to create a new event...','wp_theatre').'" />';
 			
 			echo '</form>';
+		}
+
+		function admin_footer_text ($text)
+		{
+			if ($this->is_theater_admin()) {
+			    $text = '<span id="footer-thankyou">'.
+			    		sprintf(__('Thank you for using <a href="%s">Theater for WordPress</a>.','wp_theatre'),'http://wordpress.org/plugins/theatre/').
+			    		'</span>';			
+			}
+			return $text;
 		}
 		
 	}
