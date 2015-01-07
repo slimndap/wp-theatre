5f49c498736baf1c3c954a2446b7218438617585
Jeroen Schmit <jeroen@slimndap.com>
2015-01-07 16:25:01 +0100

    Bugfix: the production-fallback for custom event field wasn't working properly. Closes #85.

--- functions/wpt_event.php
+++ functions/wpt_event.php
@@ -105,9 +105,9 @@ class WPT_Event {
 
 		if (!isset($this->{$field})) {
 			$custom_value = get_post_meta($this->ID, $field, true);
-			if (empty($custom_value)) {
-				$custom_value = get_post_meta($this->production->ID, $field, true);			
-			}
+	        if (empty($custom_value)) {
+	            $custom_value = $this->production()->custom($field, $args, true);
+	        }
 
 			$this->{$field} = apply_filters(
 				'wpt_event_'.$field, 
