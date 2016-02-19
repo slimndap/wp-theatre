5519a5444a77fc9c4377e4eb3c6d266b4b80fcd7
Jeroen Schmit <jeroen@slimndap.com>
2016-02-19 23:27:05 +0100

    Codesniffer approved!

--- functions/wpt_productions_admin.php
+++ functions/wpt_productions_admin.php
@@ -9,13 +9,13 @@ class WPT_Productions_Admin {
 	function __construct() {
 		add_action( 'admin_menu', array( $this, 'add_submenu' ) );
 		add_filter( 'wpt_production_title_html', array( $this, 'add_production_title_edit_link' ), 10, 2 );
-        add_filter('wpt/production/thumbnail/html', array($this, 'add_production_thumbnail_placeholder'), 10, 4);
+		add_filter( 'wpt/production/thumbnail/html', array( $this, 'add_production_thumbnail_placeholder' ), 10, 4 );
 	}
 
 
 	/**
 	 * Adds a thumbnail placeholder to productions without a thumnbail.
-	 * 
+	 *
 	 * @since	0.15
 	 * @param	string			$html		The production thumbnail HTML.
 	 * @param	string			$size		The thumbnail size.
@@ -23,59 +23,59 @@ class WPT_Productions_Admin {
 	 * @param	WPT_Production	$production	The production.
 	 * @return	string						The new production thumbnail HTML.
 	 */
-	function add_production_thumbnail_placeholder($html, $size, $filters, $production) {
+	function add_production_thumbnail_placeholder( $html, $size, $filters, $production ) {
 
 		// Bail if not in admin.
-		if (!is_admin()) {
+		if ( ! is_admin() ) {
 			return $html;
 		}
-		
+
 		// Bail if not on Productions Admin Page.
-		if (empty($_GET['page']) || 'theater-events' != $_GET['page']) {
+		if ( empty( $_GET['page'] ) || 'theater-events' != $_GET['page'] ) {
 			return $html;
-		}		
-		
+		}
+
 		// Add placeholder if thumbnail is empty.
-		if (empty($html)) {
+		if ( empty( $html ) ) {
 			$html = '<figure class="placeholder"><span class="dashicons dashicons-tickets-alt"></span></figure>';
 		}
-		
+
 		return $html;
 	}
 
 	/**
 	 * Adds a link to the production edit screen to the production title.
-	 * 
+	 *
 	 * @since	0.15
 	 * @param 	string			$html		The production title HTML.
 	 * @param 	WPT_Production	$production	The production.
-	 * @return	string						The production title HTML with a link to 
+	 * @return	string                      The production title HTML with a link to
 	 *										the production edit screen.
 	 */
-	function add_production_title_edit_link($html, $production) {
+	function add_production_title_edit_link( $html, $production ) {
 
 		// Bail if not in admin.
-		if (!is_admin()) {
+		if ( ! is_admin() ) {
 			return $html;
 		}
-		
+
 		// Bail if not on Productions Admin Page.
-		if (empty($_GET['page']) || 'theater-events' != $_GET['page']) {
+		if ( empty( $_GET['page'] ) || 'theater-events' != $_GET['page'] ) {
 			return $html;
 		}
-		
+
 		// Bail if Productions Admin Page is showing trashed productions.
-		if ('trash' == get_post_status($production->ID) ) {
+		if ( 'trash' == get_post_status( $production->ID ) ) {
 			return $html;
 		}
 
 		ob_start();
-		
+
 		?><div class="wp_theatre_prod_title">
 			<a href="<?php echo get_edit_post_link( $production->ID ); ?>">
 				<?php echo $production->title(); ?>
 			</a><?php
-			_post_states( $production->post() );			
+			_post_states( $production->post() );
 		?></div><?php
 
 		return ob_get_clean();
@@ -83,7 +83,7 @@ class WPT_Productions_Admin {
 
 	/**
 	 * Add a submenu for the Productions Admin Screen to the Theater menu.
-	 * 
+	 *
 	 * @since	0.15
 	 * @return 	void
 	 */
@@ -94,40 +94,40 @@ class WPT_Productions_Admin {
 			__( 'Events', 'theatre' ),
 			'edit_posts',
 			'theater-events',
-			array($this,'page_html')
+			array( $this,'page_html' )
 		);
 	}
 
 	/**
 	 * Outputs the HTML for the Productions Admin Page.
-	 * 
+	 *
 	 * @since	0.15
 	 * @see		WPT_Productions_Admin::add_submenu()
 	 * @return 	void
 	 */
 	public function page_html() {
-		
+
 		$list_table = new WPT_Productions_List_Table();
-		
+
 		ob_start();
-		
+
 		?><div class="wrap">
 			<h1><?php _e( 'Events','theatre' ); ?>
 				<a href="<?php echo admin_url( 'post-new.php?post_type='.WPT_Production::post_type_name );?>" class="page-title-action">
-					<?php _e('Add new event', 'theatre'); ?>
+					<?php _e( 'Add new event', 'theatre' ); ?>
 				</a>
 				<?php echo $this->get_search_request_summary_html(); ?>
 			</h1><?php
-		
+
 			$list_table->views();
-	
+
 			?><form method="get">
 				<input type="hidden" name="page" value="theater-events" /><?php
-	
+
 				$list_table->prepare_items();
 				$list_table->search_box( __( 'Search events', 'theatre' ), WPT_Production::post_type_name );
 				$list_table->display();
-					
+
 			?></form>
 		</div><?php
 
@@ -136,7 +136,7 @@ class WPT_Productions_Admin {
 
 	/**
 	 * Gets a summary of the search request.
-	 * 
+	 *
 	 * @since	0.15
 	 * @return 	string	A summary of the search request.
 	 */
@@ -146,13 +146,11 @@ class WPT_Productions_Admin {
 		if ( empty( $_REQUEST['s'] ) ) {
 			return '';
 		}
-		
+
 		ob_start();
 		?> <span class="subtitle"><?php
-			printf(__( 'Search results for &#8220;%s&#8221;' ),sanitize_text_field( $_REQUEST['s']));
+			printf( __( 'Search results for &#8220;%s&#8221;' ),sanitize_text_field( $_REQUEST['s'] ) );
 		?></span><?php
 		return ob_get_clean();
 	}
-
-
 }
--- functions/wpt_productions_list_table.php
+++ functions/wpt_productions_list_table.php
@@ -1,12 +1,12 @@
 <?php
 
-if(!class_exists('WP_List_Table')){
-    require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
-}	
+if ( ! class_exists( 'WP_List_Table' ) ) {
+	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+}
 
 /**
  * List Table for the Productions Admin Page (default view).
- * 
+ *
  * @since	0.15
  * @extends	WP_List_Table
  * @see		WPT_Productions_Admin
@@ -17,23 +17,23 @@ class WPT_Productions_List_Table extends WP_List_Table {
 
 	/**
 	 * Gets the HTML of the production categories for use in the category column.
-	 * 
+	 *
 	 * @since	0.15
 	 * @param 	WPT_Production	$production	The Production.
 	 * @return	string						The HTML of the production categories.
 	 */
-	function column_categories($production) {
+	function column_categories( $production ) {
 		$args = array(
 			'html' => true,
 		);
-		return $production->categories($args);
+		return $production->categories( $args );
 	}
 
-	function column_cb($production){ 
-		return sprintf( 
-			'<input type="checkbox" name="production[]" value="%1$s" />', 
+	function column_cb( $production ) {
+		return sprintf(
+			'<input type="checkbox" name="production[]" value="%1$s" />',
 			$production->ID
-		); 
+		);
 	}
 
 	/**
@@ -43,187 +43,186 @@ class WPT_Productions_List_Table extends WP_List_Table {
 	 * @param 	WPT_Production	$production	The Production.
 	 * @return	string						The HTML of the production title and actions.
 	 */
-    function column_title($production){
-	    
+	function column_title( $production ) {
+
 	    // Define the actions for this production.
-        if ('trash' == get_post_status($production->ID) ) {
-	        $restore_url = add_query_arg('action', 'untrash', get_edit_post_link( $production->ID ));
-	        $restore_url = wp_nonce_url($restore_url, 'untrash-post_'.$production->ID);
+		if ( 'trash' == get_post_status( $production->ID ) ) {
+	        $restore_url = add_query_arg( 'action', 'untrash', get_edit_post_link( $production->ID ) );
+	        $restore_url = wp_nonce_url( $restore_url, 'untrash-post_'.$production->ID );
 	        $actions = array(
-    	        'restore' 	=> '<a href="'.$restore_url.'">'.__('Restore').'</a>',
-				'delete' => '<a href="'.get_delete_post_link( $production->ID, '', true ).'">'.__('Delete Permanently').'</a>',
-			);		    		        		        
-        } else {
+		        'restore' 	=> '<a href="'.$restore_url.'">'.__( 'Restore' ).'</a>',
+				'delete' => '<a href="'.get_delete_post_link( $production->ID, '', true ).'">'.__( 'Delete Permanently' ).'</a>',
+			);
+		} else {
 	        $actions = array(
-    	        'edit' 	=> '<a href="'.get_edit_post_link( $production->ID ).'">'.__('Edit').'</a>',
-				'delete' => '<a href="'.get_delete_post_link( $production->ID, '', false ).'">'.__('Trash').'</a>',
-				'view' => '<a href="'.get_permalink($production->ID).'">'.__('View').'</a>',
-			);		    		        
-        }
-        
-        // Set the production template.
-        $production_args = array(
-	    	'template' => '{{thumbnail}}{{title}}{{dates}}{{cities}}',  
-        );
-        
-        return sprintf('%1$s %2$s',
-            $production->html($production_args),
-            $this->row_actions($actions)
-        );
-    }
-    
-    
+		        'edit' 	=> '<a href="'.get_edit_post_link( $production->ID ).'">'.__( 'Edit' ).'</a>',
+				'delete' => '<a href="'.get_delete_post_link( $production->ID, '', false ).'">'.__( 'Trash' ).'</a>',
+				'view' => '<a href="'.get_permalink( $production->ID ).'">'.__( 'View' ).'</a>',
+			);
+		}
+
+		// Set the production template.
+		$production_args = array(
+	    	'template' => '{{thumbnail}}{{title}}{{dates}}{{cities}}',
+		);
+
+		return sprintf('%1$s %2$s',
+			$production->html( $production_args ),
+			$this->row_actions( $actions )
+		);
+	}
+
+
 	/**
 	 * Gets the bulk actions for the productions.
-	 * 
+	 *
 	 * @since	0.15
 	 * @return	array	The bulk actions.
 	 */
 	function get_bulk_actions() {
-		
-		if (!empty($_REQUEST['post_status']) && 'trash' == $_REQUEST['post_status']) {
+
+		if ( ! empty( $_REQUEST['post_status'] ) && 'trash' == $_REQUEST['post_status'] ) {
 			$actions = array(
-				'restore' => __('Restore'),
-				'delete' => __('Delete Permanently'),
+				'restore' => __( 'Restore' ),
+				'delete' => __( 'Delete Permanently' ),
 			);
 		} else {
 			$actions = array(
-				'publish' => __('Publish'),
-				'draft' => __('Save as Draft', 'theatre'),
-				'trash' => __('Move to Trash'),
+				'publish' => __( 'Publish' ),
+				'draft' => __( 'Save as Draft', 'theatre' ),
+				'trash' => __( 'Move to Trash' ),
 			);
 		}
 
 		return $actions;
-	}	
+	}
 
-	
+
 	/**
 	 * Gets the columns for the list table.
-	 * 
+	 *
 	 * @since	0.15
 	 * @return	array	The columns.
 	 */
-	function get_columns(){
-        $columns = array(
-            'cb' => '<input type="checkbox" />',
-            'title' => __('Event', 'theatre'),
-            'categories' => __('Categories', 'theatre'),
-        );
-        return $columns;
-    }
+	function get_columns() {
+		$columns = array(
+			'cb' => '<input type="checkbox" />',
+			'title' => __( 'Event', 'theatre' ),
+			'categories' => __( 'Categories', 'theatre' ),
+		);
+		return $columns;
+	}
 
 	/**
 	 * Gets the sortable columns for the list table.
-	 * 
+	 *
 	 * @since	0.15
 	 * @return	array	The sortable columns.
 	 */
-	function get_sortable_columns() { 
-		$sortable_columns = array( 
-			'title' => array('title',false),
-		); 
-		return $sortable_columns; 
+	function get_sortable_columns() {
+		$sortable_columns = array(
+			'title' => array( 'title',false ),
+		);
+		return $sortable_columns;
 	}
-    
-    /**
-     * Gets the views for the list table.
-     * 
+
+	/**
+	 * Gets the views for the list table.
+	 *
 	 * @since	0.15
 	 * @return	array	The views.
-     */
-    function get_views() {
-	    $num_posts = wp_count_posts(WPT_Production::post_type_name, 'readable');
+	 */
+	function get_views() {
+	    $num_posts = wp_count_posts( WPT_Production::post_type_name, 'readable' );
 
 	    $views = array();
 
 		ob_start();
-		?><a href="<?php echo add_query_arg('post_status', '');?>" <?php
-			if (empty($_REQUEST['post_status'])) {
-				?> class="current"<?php
-			}
-		?>><?php _e('All'); ?></a><?php		
+		?><a href="<?php echo add_query_arg( 'post_status', '' );?>" <?php
+if ( empty( $_REQUEST['post_status'] ) ) {
+	?> class="current"<?php
+}
+		?>><?php _e( 'All' ); ?></a><?php
 	    $views['all'] = ob_get_clean();
-	    
+
 		$views_available = array(
-			'publish' => __('Published'),
-			'draft' => __('Draft'),
-			'trash' => __('Trash'),
+			'publish' => __( 'Published' ),
+			'draft' => __( 'Draft' ),
+			'trash' => __( 'Trash' ),
 		);
-	    
-	    foreach ($views_available as $key=>$val) {
-		    if (!empty($num_posts->{$key})) {
+
+	    foreach ( $views_available as $key => $val ) {
+		    if ( ! empty( $num_posts->{$key} ) ) {
 				ob_start();
-				?><a href="<?php echo add_query_arg('post_status', $key);?>" <?php
-					if (!empty($_REQUEST['post_status']) && $key == $_REQUEST['post_status']) {
-						?> class="current"<?php
-					}
-				?>><?php echo $val; ?></a><?php		
-			    $views[$key] = ob_get_clean();
-		    } 
+				?><a href="<?php echo add_query_arg( 'post_status', $key );?>" <?php
+if ( ! empty( $_REQUEST['post_status'] ) && $key == $_REQUEST['post_status'] ) {
+	?> class="current"<?php
+}
+				?>><?php echo $val; ?></a><?php
+			    $views[ $key ] = ob_get_clean();
+		    }
 	    }
 	    return $views;
-    }
+	}
 
-    /**
-     * Queries and filters the productions, handles sorting, and pagination, and any 
-     * other data-manipulation required prior to rendering.
-     *
-     * @since	0.15
-     * @return 	void
-     */
-    function prepare_items() {
+	/**
+	 * Queries and filters the productions, handles sorting, and pagination, and any
+	 * other data-manipulation required prior to rendering.
+	 *
+	 * @since	0.15
+	 * @return 	void
+	 */
+	function prepare_items() {
 	    global $wp_theatre;
 
 	    $per_page = 10;
 
 	    $columns = $this->get_columns();
 		$hidden = array();
-        $sortable = $this->get_sortable_columns();
-        $this->_column_headers = array($columns, $hidden, $sortable);
-	    
+		$sortable = $this->get_sortable_columns();
+		$this->_column_headers = array( $columns, $hidden, $sortable );
+
 	    $production_args = array();
-	    
+
 	    $production_args['status'] = 'any';
-	    
-	    if (!empty($_REQUEST['orderby']) && !empty($_REQUEST['order']) && 'title' == $_REQUEST['orderby']) {
+
+	    if ( ! empty( $_REQUEST['orderby'] ) && ! empty( $_REQUEST['order'] ) && 'title' == $_REQUEST['orderby'] ) {
 		    $production_args['order'] = $_REQUEST['order'];
 	    }
-	    
-	    if (!empty($_REQUEST['cat']) && is_numeric($_REQUEST['cat'])) {
+
+	    if ( ! empty( $_REQUEST['cat'] ) && is_numeric( $_REQUEST['cat'] ) ) {
 		    $production_args['cat'] = $_REQUEST['cat'];
 	    }
-	    
-	    if (!empty($_REQUEST['dates']) && 'upcoming' == $_REQUEST['dates']) {
+
+	    if ( ! empty( $_REQUEST['dates'] ) && 'upcoming' == $_REQUEST['dates'] ) {
 		    $production_args['start'] = 'now';
 	    }
-	    
-	    if (!empty($_REQUEST['post_status'])) {
-		    $production_args['status'] = array($_REQUEST['post_status']);
+
+	    if ( ! empty( $_REQUEST['post_status'] ) ) {
+		    $production_args['status'] = array( $_REQUEST['post_status'] );
 	    }
-	    
-	    if (!empty($_REQUEST['s'])) {
-		    $production_args['s'] = sanitize_text_field($_REQUEST['s']);
+
+	    if ( ! empty( $_REQUEST['s'] ) ) {
+		    $production_args['s'] = sanitize_text_field( $_REQUEST['s'] );
 	    }
-	    
-	 	$productions = $wp_theatre->productions->get($production_args);
+
+	 	$productions = $wp_theatre->productions->get( $production_args );
 
 	    $current_page = $this->get_pagenum();
-	    
-	 	$total_items = count($productions);
-	 	
-	 	$productions = array_slice($productions,(($current_page-1)*$per_page),$per_page);
+
+	 	$total_items = count( $productions );
+
+	 	$productions = array_slice( $productions,(($current_page -1) * $per_page),$per_page );
 
 	 	$this->items = $productions;
-	 	
-	 	$this->set_pagination_args( 
-	 		array( 
-	 			'total_items' => $total_items, 
-	 			'per_page' => $per_page, 
-	 			'total_pages' => ceil($total_items/$per_page)
+
+	 	$this->set_pagination_args(
+	 		array(
+	 			'total_items' => $total_items,
+	 			'per_page' => $per_page,
+	 			'total_pages' => ceil( $total_items / $per_page ),
 	 		)
 	 	);
-	 	
+
 	}
-    
 }
