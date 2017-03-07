e66754eb81e00f1455d0a1ff5881a84b95edf9aa
Jeroen Schmit <jeroen@slimndap.com>
2017-03-07 15:10:57 +0100

    Bump to 0.15.17.

--- theater.php
+++ theater.php
@@ -5,7 +5,7 @@
 	Plugin URI: https://wp.theater/
 	Description: Turn your Wordpress website into a theater website.
 	Author: Jeroen Schmit
-	Version: 0.15.16
+	Version: 0.15.17
 	Author URI: http://slimndap.com/
 	Text Domain: theatre
 	Domain Path: /lang
@@ -28,7 +28,7 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 	
-$wpt_version = '0.15.16';
+$wpt_version = '0.15.17';
 
 class WP_Theatre {
 	function __construct() {
