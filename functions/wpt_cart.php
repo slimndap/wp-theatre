2ebc898810f82c449c3a2641ad271d134b7381a0
Jeroen Schmit <jeroen@slimndap.com>
2017-03-27 11:48:27 +0200

    Added inline docs to WPT_Cart. Fixes #238. Fixes #64. Removed previous fix for #64.

--- functions/wpt_cart.php
+++ functions/wpt_cart.php
@@ -1,5 +1,6 @@
 <?php
 class WPT_Cart {
+	
 	function __construct() {
 		add_action('init',array($this,'init'));
 	}
@@ -10,23 +11,41 @@ class WPT_Cart {
 	 * Set items and amount from the current session.
 	 *
 	 * @since	0.?
-	 * @since	0.15.17
+	 * @since	0.15.22	No longer starts unnecessary sessions.
+	 *					Fixes #238.
+	 *
+	 * @uses	WPT_Cart::session_start() to start a new session.
 	 * @return 	void
 	 */
 	function init() {
 
+		/**
+		 * Bail if no session has started yet to avoid unnecessary session_start().
+		 * See: https://github.com/slimndap/wp-theatre/issues/238
+		 * See: http://stackoverflow.com/a/1783257
+		 */
 		if ( !isset( $_COOKIE[ session_name() ] ) ) {
 			return;
 		}
 	
+		// Start a new session.
 		$this->session_start();
         
+        // Import cart contents from session.
 		if (isset($_SESSION['wpt_cart'])) {
 			$this->items = $_SESSION['wpt_cart']['items'];
 			$this->amount = $_SESSION['wpt_cart']['amount'];
 		}
 	}
 	
+	/**
+	 * Saves the cart contents to the session.
+	 * 
+	 * @since	0.?
+	 *
+	 * @uses	WPT_Cart::session_start() to start a new session.
+	 * @return 	void
+	 */
 	function save() {
 
 		$this->session_start();
@@ -38,15 +57,22 @@ class WPT_Cart {
 		
 	}
 	
+	
+	/**
+	 * Resets the cart by emptying the cart contents and destroying the session.
+	 * 
+	 * @since	0.?
+	 *
+	 * @return void
+	 */
 	function reset() {
 
 		$this->session_start();
 		
+		// Empty the cart contents.
 		$this->items = array();
 		$this->amount = 0;
-		
-		//$_SESSION = array();
-		
+
 		// If it's desired to kill the session, also delete the session cookie.
 		// Note: This will destroy the session, and not just the session data!
 		if (ini_get("session.use_cookies")) {
@@ -59,24 +85,51 @@ class WPT_Cart {
 		
 	}
 	
+	
+	/**
+	 * Checks if the cart is emtpy.
+	 * 
+	 * @since	0.?
+	 *
+	 * @return 	bool
+	 */
 	function is_empty() {		
 		return empty($this->items);
 	}
 	
+	/**
+	 * Adds a production item to the cart.
+	 * 
+	 * @since	0.?
+	 *
+	 * @param 	WPT_Production 	$production
+	 * @return 	void
+	 */
 	function add_item($production) {
-		// check if production is already present in cart
+		
+		// Update item count if production is already present in cart.
 		for ($i=0;$i<count($this->items);$i++) {
 			if ($this->items[$i]['production']->ID==$production->ID) {
 				$this->items[$i]['amount']++;
 				return;
 			}
 		}
+		
+		// Add production to cart (if not present in cart yet).
 		$this->items[] = array(
 			'amount'=>1,
 			'production'=>$production
 		);
+		
 	}
 	
+	/**
+	 * Outputs the cart HTML.
+	 * 
+	 * @since	0.?
+	 *
+	 * @return	string
+	 */
 	function render() {
 		$html = '';
 		if (!$this->is_empty()) {
@@ -87,11 +140,25 @@ class WPT_Cart {
 				$html.= '</div>'; // .wpt_cart_item				
 			}
 		}
+			
+		/**
+		 * Filter the cart HTML.
+		 * 
+		 * @since	0.?
+		 *
+		 * @var 	string	$html	The cart HTML.
+		 */
 		$html = apply_filters('wpt_cart', $html);
 
 		return $html;
 	}
 	
+	/**
+	 * Starts a new session if no session has started yet.
+	 * 
+	 * @since	0.15.22
+	 * @return	void
+	 */
 	function session_start() {
 		if ( PHP_SESSION_NONE === session_status() ) {
 			//session_name('Theater_Cart');
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,18 +8,6 @@ require_once $_tests_dir . '/includes/functions.php';
 function _manually_load_plugin() {
 	global $wp_theatre;
 
-	/*
-	 * Fake a session_start() to avoid PHP Warnings when
-	 * Theater tries to start a new session.
-	 * 
-	 * @since 0.9.5
-	 *
-	 * @see WPT_Cart::init()
-	 * @see https://github.com/slimndap/wp-theatre/issues/67
-	 * @see http://stackoverflow.com/a/23400788
-	 */	 
-	@session_start();
-
 	require dirname( __FILE__ ) . '/../theater.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
