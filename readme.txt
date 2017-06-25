58da8eaff858ae5909d8b66bd7e905c913707a79
Jeroen Schmit <jeroen@slimndap.com>
2017-06-25 13:21:48 +0200

    Ready for 0.15.28.

--- readme.txt
+++ readme.txt
@@ -247,6 +247,9 @@ Release Date: May 30th, 2015
 
 == Upgrade Notice ==
 
+= 0.15.28 =
+Fixes a problem with the Categories widget that was introduced in a previous update.
+
 = 0.15.27 =
 Added a new `{{tags}}` placeholder for events.
 
--- theater.php
+++ theater.php
@@ -5,7 +5,7 @@
 	Plugin URI: https://wp.theater/
 	Description: Turn your Wordpress website into a theater website.
 	Author: Jeroen Schmit
-	Version: 0.15.27
+	Version: 0.15.28
 	Author URI: http://slimndap.com/
 	Text Domain: theatre
 	Domain Path: /lang
@@ -28,7 +28,7 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 	
-$wpt_version = '0.15.27';
+$wpt_version = '0.15.28';
 
 class WP_Theatre {
 	function __construct() {
