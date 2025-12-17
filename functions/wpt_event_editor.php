b0003fd8a71af681e630802741d63b84b2bf922a
Jeroen Schmit <jeroen@slimndap.com>
2025-12-17 14:28:20 +0100

    Add capability check for AJAX event creation

--- functions/wpt_event_editor.php
+++ functions/wpt_event_editor.php
@@ -72,6 +72,7 @@ class WPT_Event_Editor {
 	 * @since	0.11.5
 	 * @since 	0.12	Event now inherits post_status from production.
 	 *					Fixes #141.
+	 * @since 	0.19.1	Requires capability to edit the production before creating events.
 	 */
 	public function create_event_over_ajax() {
 
@@ -79,16 +80,22 @@ class WPT_Event_Editor {
 
 		parse_str( $_POST['post_data'], $post_data );
 
+		$production_id = isset( $post_data['post_ID'] ) ? absint( $post_data['post_ID'] ) : 0;
+
+		if ( empty( $production_id ) || ! current_user_can( 'edit_post', $production_id ) ) {
+			wp_die( __( 'You are not allowed to create dates for this production.', 'theatre' ) );
+		}
+
 		if ( ! empty($post_data['wpt_event_editor_event_date']) ) {
 
-			$post = array(
-				'post_type' => WPT_Event::post_type_name,
-				'post_status' => get_post_status( $post_data['post_ID'] ),
-			);
+				$post = array(
+					'post_type' => WPT_Event::post_type_name,
+					'post_status' => get_post_status( $production_id ),
+				);
 
-			if ( $event_id = wp_insert_post( $post ) ) {
+				if ( $event_id = wp_insert_post( $post ) ) {
 
-				add_post_meta( $event_id, WPT_Production::post_type_name, $post_data['post_ID'], true );
+					add_post_meta( $event_id, WPT_Production::post_type_name, $production_id, true );
 
 				foreach ( $this->get_fields( $event_id ) as $field ) {
 					$this->save_field( $field, $event_id, $post_data );
@@ -96,7 +103,7 @@ class WPT_Event_Editor {
 			}
 		}
 
-		echo $this->get_listing_html( $post_data['post_ID'] );
+			echo $this->get_listing_html( $production_id );
 
 		wp_die();
 	}
--- tests/test_event_editor.php
+++ tests/test_event_editor.php
@@ -402,6 +402,40 @@ class WPT_Test_Event_Editor_Ajax extends WP_Ajax_UnitTestCase {
 
 	}
 
+	/**
+	 * Regression coverage for missing capability check on event creation.
+	 * Verifies subscribers can (currently) create events via wpt_event_editor_create_event.
+	 * Documented vulnerability report (Patchstack) until capability check is added.
+	 */
+	function test_subscriber_cannot_create_event_over_ajax() {
+		$production_id = $this->create_production();
+
+		$this->_setRole( 'subscriber' );
+
+		$_POST['nonce'] = wp_create_nonce( 'wpt_event_editor_ajax_nonce' );
+
+		$event_date = date( 'Y-m-d H:i', time() + WEEK_IN_SECONDS );
+		$post_data = array(
+			'wpt_event_editor_nonce' => wp_create_nonce( 'wpt_event_editor' ),
+			'wpt_event_editor_event_date' => $event_date,
+			'post_ID' => $production_id,
+		);
+		$_POST['post_data'] = http_build_query( $post_data );
+
+			try {
+				$this->_handleAjax( 'wpt_event_editor_create_event' );
+			} catch ( WPAjaxDieStopException $e ) {
+				// Expected hard die for unauthorized user.
+			} catch ( WPAjaxDieContinueException $e ) {
+				// Expected die for AJAX handler.
+			}
+
+		$production = new WPT_Production( $production_id );
+		$events = $production->events();
+
+		$this->assertCount( 0, $events );
+	}
+
 	function test_event_value_can_be_emptied() {
 
 		// Create a production with an event.
